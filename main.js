131b20c2ca730b453c08d8716a7b40c2fd29e197
Jnapthi <73169558+jnapthim@users.noreply.github.com>
2021-07-19 12:59:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var firstname="Zoya"
+var lastname="Rana"
+var fullname=firstname.concat(lastname)
+var text="My First Console Project"
+var number1="15"
+var number2="5"
+var number3="50"
+var number4="2"
+console.log(fullname);
+console.log(text);
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
